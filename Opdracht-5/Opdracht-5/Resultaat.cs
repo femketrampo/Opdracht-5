509b469f9cf3e28907cfe20016e1124b85d94c33
femketrampo <76632910+femketrampo@users.noreply.github.com>
2022-03-22 09:50:13 +0100

    Add files via upload

---
+++ Opdracht-5/Opdracht-5/Resultaat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opdracht_5
+{
+    class Resultaat
+    {
+        public string Naam;
+        public double Nederlands;
+        public double Frans;
+        public double Engels;
+    }
+}
